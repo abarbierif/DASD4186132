0ee334f7e35b41b07213d446d6ba26c2047ba7e8
abarbierif <angelo.bf8@gmail.com>
2026-05-24 16:20:56 -0400

    Fix number of samples logic with 1016 as maximum insted of 116, remove repeated entry in div lut

--- assignment02/src/hdl/acquisition_thread.sv
+++ assignment02/src/hdl/acquisition_thread.sv
@@ -119,11 +119,12 @@ module acquisition_thread(
       m_samples <= 0;
     end else begin
       if(samples_fsm_ready) begin
-        n_samples <= n;
-	    if(n >= 10'd115) begin
-        m_samples <= 11'd1024;
-	    end else begin
-	      m_samples <= n + 10'd8;
+	      if(n > 10'd1016) begin
+          m_samples <= 11'd1024;
+          n_samples <= 10'd1016;
+	      end else begin
+	        m_samples <= n + 10'd8;
+          n_samples <= n;
         end
       end
     end
